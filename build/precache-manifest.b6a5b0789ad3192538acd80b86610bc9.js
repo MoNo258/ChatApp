aa32991895fde8fa7f24cc218d1bddee9f7e16cf
Monika N <monika258@gmail.com>
2020-07-14 23:51:33 +0200

    build added

---
+++ build/precache-manifest.b6a5b0789ad3192538acd80b86610bc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa87264d30f66eb31073623bed604274",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04fb5d17c5c5003e3e7f",
+    "url": "/static/css/main.18879f3f.chunk.css"
+  },
+  {
+    "revision": "1070b88ec9f2840d49f3",
+    "url": "/static/js/2.d4c2849f.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.d4c2849f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04fb5d17c5c5003e3e7f",
+    "url": "/static/js/main.5a422a72.chunk.js"
+  },
+  {
+    "revision": "241f87120d285ee26494",
+    "url": "/static/js/runtime-main.56ae10a7.js"
+  }
+]);
